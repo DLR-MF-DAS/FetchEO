472e81036fb070ddebec5e358656568076b8ae74
Samuel Garske <samuel.garske@dlr.de>
2026-04-24 10:57:08 +0000

    Updated bands to L2 available only.

--- src/fetcheo/downloaders/sen3_openeo.py
+++ src/fetcheo/downloaders/sen3_openeo.py
@@ -165,8 +165,8 @@ class Sen3WaterOpenEODownloader(BaseDownloader):
         """Returns all available bands for Sentinel-3 OLCI Level-2 Water."""
         return [
             "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B09", 
-            "B10", "B11", "B12", "B13", "B14", "B15", "B16", "B17", "B18",
-            "B19", "B20", "B21", "FLAGS", "IWV", "CHL_OC4ME", "TSM_NN", 
+            "B10", "B11", "B12", "B16", "B17", "B18",
+            "B21", "FLAGS", "IWV", "CHL_OC4ME", "TSM_NN", 
             "PAR", "KD490_M07", "A865", "T865", "CHL_NN", "ADG443_NN"
         ]
 
